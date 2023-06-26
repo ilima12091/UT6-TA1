68dc8198653fdf08e487328456f4268580a22c86
ignacio.lima <ignacio.lima@inswitch.com>
2023-06-26 15:43:42 -0300

    Added delete card feature

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,18 +1,23 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Card } from '../interfaces/card';
+import { CardsService } from '../services/cards-service.service';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.scss'],
 })
-export class CardComponent implements OnInit {
+export class CardComponent {
   @Input() card!: Card;
   @Input() setCardToEdit!: (card: Card) => void;
   moviesImagesBaseUrl: string = 'https://image.tmdb.org/t/p/w500';
   isEdittingModeEnabled: boolean = false;
 
-  constructor() {}
+  constructor(private cardsService: CardsService) {}
 
-  ngOnInit() {}
+  deleteCard() {
+    this.cardsService.deleteCard(this.card.id!).then((result) => {
+      if (result) location.reload();
+    });
+  }
 }
